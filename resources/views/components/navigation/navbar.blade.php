22add084a925eac7e66bf8235e4e7f6c3d2448ed
rahman <rahmannurhayadi@gmail.com>
2024-05-08 14:45:33 +0700

    fix : fix url

--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -1,9 +1,9 @@
-<nav class="w-full h-16 bg-indigo-800 flex justify-between items-center p-4 text-white font-semibold">
+<nav class="w-full h-14 bg-indigo-800 flex justify-between items-center p-4 text-white font-semibold font-deca">
     <h1 class="text-xl font-bold">Catalog <span class="text-black">X</span></h1>
     <div class="flex justify-between items-center gap-x-6 te">
         <ul class="flex justify-evenly items-center gap-x-2 w-56">
-            <li><a href="/products">Products</a></li>
-            <li><a href="/categories">Categories</a></li>
+            <li><a href="/users/shop">Shop</a></li>
+            <li><a href="/users/products">Products</a></li>
         </ul>
         <button>Profile</button>
     </div>
