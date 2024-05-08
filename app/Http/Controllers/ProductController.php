41bfe69b75987273c9bea5ecb6f2dab5181bb174
rahman <rahmannurhayadi@gmail.com>
2024-05-08 14:44:28 +0700

    feat : add new func return shop view

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
+    public function getShop(){
+        return view('products.shops');
+    }
     public function getProducts(){
         return view('products.product');
     }
