df5e2fbaca7b91ae21cefa9cdabe10d275fa4f2b
rahman <rahmannurhayadi@gmail.com>
2024-05-14 11:20:24 +0700

    fix : change uri

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\StoresController;
 
 Route::get('/', function(){
     return redirect()->route('users.login');
@@ -11,8 +12,11 @@ Route::get('/', function(){
 Route::name('users.')->group(function(){
     Route::get('/users/login',[UserController::class,'login'])->name('login');
     Route::get('/users/register',[UserController::class,'register'])->name('register');
-    Route::get('/users/shop',[ProductController::class,'getShop']);
-    Route::get('/users/products',[ProductController::class,'getProducts']);
+    Route::get('/users/profile/username',[UserController::class,'profile'])->name(('profile'));
+    Route::get('/users/profile/catalogs',[ProductController::class,'getCatalogs'])->name('catalogs');
 });
+
+Route::get('/stores',[StoresController::class,'getStores']);
+;
 
 
