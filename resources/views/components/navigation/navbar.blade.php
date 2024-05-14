4943208f90b4c72d320eabdffff3aed26f5c316d
rahman <rahmannurhayadi@gmail.com>
2024-05-14 11:16:28 +0700

    fix : fix nav link

--- resources/views/components/navigation/navbar.blade.php
+++ resources/views/components/navigation/navbar.blade.php
@@ -1,10 +1,13 @@
-<nav class="w-full h-14 bg-indigo-800 flex justify-between items-center p-4 text-white font-semibold font-deca">
+<nav class="w-full h-14 bg-indigo-800 flex justify-between items-center p-4 text-white font-semibold font-deca fixed">
     <h1 class="text-xl font-bold">Catalog <span class="text-black">X</span></h1>
     <div class="flex justify-between items-center gap-x-6 te">
         <ul class="flex justify-evenly items-center gap-x-2 w-56">
-            <li><a href="/users/shop">Shop</a></li>
-            <li><a href="/users/products">Products</a></li>
+            <li class="p-2 rounded hover:bg-white/30 transition-colors duration-300 {{ request()->is('stores') ? 'bg-black/50' : 'bg-transparent' }}">
+                <a href="/stores">Stores</a>
+            </li>
+            <li class="p-2 rounded hover:bg-white/30 transition-colors duration-300 {{ request()->is('profile/username') ? 'bg-black/50' : 'bg-transparent' }}">
+                <a href="/users/profile/username">Rahman</a>
+            </li>
         </ul>
-        <button>Profile</button>
     </div>
 </nav>
