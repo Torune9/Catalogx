515bef4b2d8bbb53c751d006c28d040ec8152b25
rahman <rahmannurhayadi@gmail.com>
2024-05-16 10:03:04 +0700

    fix :  delete unused field

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,10 +15,14 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->integer('role_id')->default(1);
             $table->timestamps();
+        });
+
+        Schema::create('role',function(Blueprint $table){
+            $table->id();
+            $table->string('role');
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
