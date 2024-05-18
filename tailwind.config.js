56d9dcd178dbf867c5d04d3f27c8d53fbf503aca
rahman <rahmannurhayadi@gmail.com>
2024-05-18 18:16:18 +0700

    feat : adding lib flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,7 @@ export default {
     "./resources/**/*.blade.php",
     "./resources/**/*.js",
     "./resources/**/*.vue",
+    "./node_modules/flowbite/**/*.js"
   ],
   theme: {
     extend: {
@@ -15,5 +16,7 @@ export default {
       }
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
