a314b3ba0f7b8f9d295f098a9a9f95abe02ce0cf
rahman <rahmannurhayadi@gmail.com>
2024-05-20 18:42:24 +0700

    feat : adding createStore func

--- app/Http/Controllers/StoresController.php
+++ app/Http/Controllers/StoresController.php
@@ -3,10 +3,29 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Stores;
+use Illuminate\Validation\Rules\File;
 
 class StoresController extends Controller
 {
-    public function getStores(){
+    public function getShops(){
         return view('products.shops');
+    }
+    
+    public function getStores(){
+        return view('products.stores');
+    }
+
+    public function createStore(Request $request){
+        $request->validate([
+            'name'=>'required|min:8',
+            'date' => 'required|min:9',
+            'image' => [
+                'required',
+                'image',
+                File::types(['jpg', 'png', 'jpeg'])->max(5 * 1024)
+            ]
+        ]);
+        Stores::create([]);
     }
 }
