95eb379d4c55e871269ada64eb67001ad127da2f
rahman <rahmannurhayadi@gmail.com>
2024-05-20 18:43:44 +0700

    fix : delete email verified at field

--- app/Models/User.php
+++ app/Models/User.php
@@ -40,7 +40,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
