e25f5de5fbafe07000b1042bba8f844205b7702e
rahman <rahmannurhayadi@gmail.com>
2024-05-28 18:38:10 +0700

    fix : fix data layout shops

--- resources/views/products/shops.blade.php
+++ resources/views/products/shops.blade.php
@@ -13,47 +13,39 @@
                 class="bg-red-700 p-2 rounded-r text-sm text-white border border-l-0  group-hover:border-black transition-colors duration-300 hover:bg-red-500">search</button>
         </form>
     </div>
+    <div class="w-full flex flex-row justify-evenly pb-10 border-b">
+        <div>
+            <label for="category">
+                All Categories
+            </label>
+            <input type="radio" value="" name="category" checked class="cursor-pointer">
+        </div>
+        @foreach ($categories as $category)
+            <div>
+                <input type="radio" name="category" id="category" value="{{ $category->id }}"class="cursor-pointer" on>
+                <label for="{{ $category->name }}">{{ $category->name }}</label>
+            </div>
+        @endforeach
+    </div>
+    @foreach ( $stores as $store )
     <div id="default-carousel" class="relative w-96 shadow-lg rounded overflow-hidden p-4" data-carousel="slide">
         <!-- Carousel wrapper -->
         <div class="relative h-56 overflow-hidden rounded">
+            @foreach ($store->catalogs as $catalog)
+            {{-- @dd($catalog->image) --}}
             <!-- Item 1 -->
             <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/car.jpg') }}"
+                <img src="{{ asset('storage/img/uploads/'. $catalog->image) }}"
                     class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
             </div>
-            <!-- Item 2 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/sepatu2.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 3 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?food"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 4 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?street"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 5 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?fashion"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
+            @endforeach
         </div>
         <!-- Slider indicators -->
         <div class="absolute z-30 w-full flex gap-x-1 justify-center top-48">
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
-                data-carousel-slide-to="0"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
-                data-carousel-slide-to="1"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
-                data-carousel-slide-to="2"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4"
-                data-carousel-slide-to="3"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5"
-                data-carousel-slide-to="4"></button>
+             @foreach ($store->catalogs as $catalog->image)
+             <button type="button" class="w-3 h-3 rounded-full"
+                 data-carousel-slide-to="1"></button>
+             @endforeach
         </div>
         <!-- Slider controls -->
         <button type="button"
@@ -85,191 +77,19 @@
         {{-- Info Toko --}}
         <div class="p-2 flex flex-col gap-2">
             <h1 class="font-bold text-2xl hover:underline font-deca">
-                <a href="{{ route('users.details') }}">
-                    Food Strict
+                <a href="{{ route('users.details',['id'=>$store->id]) }}">
+                    {{ $store->name }}
                 </a>
             </h1>
-            <h2 class="text-sm font-semibold text-green-600">Owner : Rahman Nurhayadi</h2>
-            <h3 class="text-[11px]">Since : 2020</h3>
+            <h3 class="text-[11px]">Since : {{ (new DateTime($store->from_date))->format('Y') }}</h3>
             <p class="text-justify text-sm hover:underline">
-                <a href="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ratione iste, expedita hic qui
-                    molestias tempora dolore eum dolores eius!
+                <a href="{{ route('users.details',['id'=>$store->id]) }}">
+                    {{ Str::limit($store->description,100)}}
                 </a>
             </p>
         </div>
     </div>
-    <div id="default-carousel" class="relative w-96 shadow-lg rounded overflow-hidden p-4" data-carousel="slide">
-        <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded">
-            <!-- Item 1 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/car.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 2 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/sepatu2.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 3 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?food"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 4 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?street"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 5 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?fashion"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-        </div>
-        <!-- Slider indicators -->
-        <div class="absolute z-30 w-full flex gap-x-1 justify-center top-48">
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
-                data-carousel-slide-to="0"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
-                data-carousel-slide-to="1"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
-                data-carousel-slide-to="2"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4"
-                data-carousel-slide-to="3"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5"
-                data-carousel-slide-to="4"></button>
-        </div>
-        <!-- Slider controls -->
-        <button type="button"
-            class="absolute top-20 start-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none"
-            data-carousel-prev>
-            <span
-                class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M5 1 1 5l4 4" />
-                </svg>
-                <span class="sr-only">Previous</span>
-            </span>
-        </button>
-        <button type="button"
-            class="absolute top-20 end-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none"
-            data-carousel-next>
-            <span
-                class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 9 4-4-4-4" />
-                </svg>
-                <span class="sr-only">Next</span>
-            </span>
-        </button>
-        {{-- Info Toko --}}
-        <div class="p-2 flex flex-col gap-2">
-            <h1 class="font-bold text-2xl hover:underline font-deca">
-                <a href="">
-                    Food Strict
-                </a>
-            </h1>
-            <h2 class="text-sm font-semibold text-green-600">Owner : Rahman Nurhayadi</h2>
-            <h3 class="text-[11px]">Since : 2020</h3>
-            <p class="text-justify text-sm hover:underline">
-                <a href="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ratione iste, expedita hic qui
-                    molestias tempora dolore eum dolores eius!
-                </a>
-            </p>
-        </div>
-    </div>
-    <div id="default-carousel" class="relative w-96 shadow-lg rounded overflow-hidden p-4" data-carousel="slide">
-        <!-- Carousel wrapper -->
-        <div class="relative h-56 overflow-hidden rounded">
-            <!-- Item 1 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/car.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 2 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="{{ asset('img/sepatu2.jpg') }}"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 3 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?food"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 4 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?street"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-            <!-- Item 5 -->
-            <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <img src="http://source.unsplash.com/600x600?fashion"
-                    class="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2" alt="...">
-            </div>
-        </div>
-        <!-- Slider indicators -->
-        <div class="absolute z-30 w-full flex gap-x-1 justify-center top-48">
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1"
-                data-carousel-slide-to="0"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
-                data-carousel-slide-to="1"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
-                data-carousel-slide-to="2"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 4"
-                data-carousel-slide-to="3"></button>
-            <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 5"
-                data-carousel-slide-to="4"></button>
-        </div>
-        <!-- Slider controls -->
-        <button type="button"
-            class="absolute top-20 start-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none"
-            data-carousel-prev>
-            <span
-                class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="M5 1 1 5l4 4" />
-                </svg>
-                <span class="sr-only">Previous</span>
-            </span>
-        </button>
-        <button type="button"
-            class="absolute top-20 end-0 z-30 flex items-center justify-center px-4 cursor-pointer group focus:outline-none"
-            data-carousel-next>
-            <span
-                class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-black/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-                <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                        d="m1 9 4-4-4-4" />
-                </svg>
-                <span class="sr-only">Next</span>
-            </span>
-        </button>
-        {{-- Info Toko --}}
-        <div class="p-2 flex flex-col gap-2">
-            <h1 class="font-bold text-2xl hover:underline font-deca">
-                <a href="">
-                    Food Strict
-                </a>
-            </h1>
-            <h2 class="text-sm font-semibold text-green-600">Owner : Rahman Nurhayadi</h2>
-            <h3 class="text-[11px]">Since : 2020</h3>
-            <p class="text-justify text-sm hover:underline">
-                <a href="">
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta ratione iste, expedita hic qui
-                    molestias tempora dolore eum dolores eius!
-                </a>
-            </p>
-        </div>
-    </div>
+
+    @endforeach
 </main>
 @endsection
