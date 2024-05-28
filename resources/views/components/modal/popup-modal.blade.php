92bfc01f3961043cb2f494723403211714da1912
rahman <rahmannurhayadi@gmail.com>
2024-05-28 18:36:13 +0700

    fix : fix form for delete product

--- resources/views/components/modal/popup-modal.blade.php
+++ resources/views/components/modal/popup-modal.blade.php
@@ -6,7 +6,9 @@
 
 <div id="popup-modal" tabindex="-1"
     class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full">
-    <div class="relative p-4 w-full max-w-md max-h-full">
+    <form action="{{ route('product.deleteCatalog',['id'=>$id]) }}" method="POST" class="relative p-4 w-full max-w-md max-h-full">
+        @csrf
+        @method('DELETE')
         <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
             <button type="button"
                 class="absolute top-3 end-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
@@ -26,7 +28,7 @@
                 </svg>
                 <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete
                     this product?</h3>
-                <button data-modal-hide="popup-modal" type="button"
+                <button data-modal-hide="popup-modal" type="submit"
                     class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center">
                     Yes, I'm sure
                 </button>
@@ -35,5 +37,5 @@
                     cancel</button>
             </div>
         </div>
-    </div>
+    </form>
 </div>
