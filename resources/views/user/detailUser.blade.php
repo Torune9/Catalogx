cac3feb30bc3cf100823f9c550be12aa073dedc1
rahman <rahmannurhayadi@gmail.com>
2024-05-28 18:39:31 +0700

    fix : fix data detailUser

--- resources/views/user/detailUser.blade.php
+++ resources/views/user/detailUser.blade.php
@@ -8,16 +8,16 @@
             Description
         </h1>
         <h3>
-           Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis velit alias assumenda obcaecati ratione, tenetur nemo magnam expedita molestiae odio ipsam cum, nulla animi ab doloribus hic sint distinctio quod! Alias laboriosam suscipit commodi soluta dolore, cum molestias nesciunt adipisci minus tempora aliquid numquam. Voluptatum illo obcaecati, sunt quia facere dolore aut, amet, repellendus sed harum corporis atque animi ipsa?
+           {{ $store->description }}
         </h3>
     </div>
     <div class="absolute w-full">
         <div class="absolute top-60 left-28 flex flex-row gap-2 justify-center items-center">
             <div class="w-60 h-60 overflow-hidden flex justify-center items-center rounded border-4">
-                <img class="h-full w-full" src="https://source.unsplash.com/600x600?profile" alt="">
+                <img class="h-full w-full" src="{{ asset('storage/img/uploads/profile/'.$store->image) }}" alt="">
             </div>
             <h1 class="w-96 text-4xl font-bold">
-                Food Strict
+                {{ $store->name }}
             </h1>
         </div>
     </div>
@@ -27,26 +27,36 @@
    </div>
    {{-- Catalog user lists --}}
    <div class="flex flex-wrap justify-center gap-4 w-full h-full mt-40 font-poppins border-t p-2">
+    @foreach ($products as $catalog)
     <div class="shadow-md p-2 rounded-t overflow-hidden w-64 h-78 flex justify-center flex-col items-center relative">
-        <x-modal.popup-modal></x-modal.popup-modal>
-        <div class="h-3/4 w-3/4 flex justify-center items-center overflow-hidden rounded-full">
-            <picture>
-                <img class="h-full w-full" src="https://source.unsplash.com/600x600?food" alt="">
+        <div class="h-40 w-40 flex justify-center items-center overflow-hidden rounded-full">
+            <picture class="h-full w-full">
+
+                @if(filter_var($catalog->image, FILTER_VALIDATE_URL))
+                <img class="h-full w-full object-cover" src="{{ $catalog->image }}" alt="">
+
+                @elseif($catalog->image)
+                <img class="h-full w-full object-cover" src="{{ asset('storage/img/uploads/'.$catalog->image) }}"
+                    alt="">
+                @endif
             </picture>
         </div>
-        <div class="p-2 font-deca w-full">
-            <h1 class="font-black text-center">FOOD</h1>
-            <p>Rp 1.000.000</p>
-            <p>Stock:200</p>
+        <div class="p-2 w-full font-poppins">
+            <h1 class="font-black text-center">{{ $catalog->name }}</h1>
+            <p class="text-sm">
+                Harga : Rp {{ number_format($catalog->price, 0, ',', '.') }}
+            </p>
+            <p class="text-sm">Stock : {{ $catalog->stock }}</p>
         </div>
         <div class="w-full text-right">
-            <button class="font-deca bg-green-800 text-sm p-2 w-40 rounded text-white font-semibold">
-                <a href="{{ route('product.updateCatalog') }}">
+            <a href="">
+                <button class="font-deca bg-green-800 text-sm p-2 w-40 rounded text-white font-semibold">
                     order
-                </a>
-            </button>
+                </button>
+            </a>
         </div>
     </div>
+    @endforeach
 </div>
 </main>
 @endsection
