b8e3b3a87ddf1c8cd2a6fc2e6f896057b4b55bb6
rahman <rahmannurhayadi@gmail.com>
2024-05-28 18:32:21 +0700

    fix: fix variable for data bind component

--- app/View/Components/catalogModal.php
+++ app/View/Components/catalogModal.php
@@ -12,7 +12,8 @@ class catalogModal extends Component
      * Create a new component instance.
      */
     public function __construct(
-        public string $data,
+        public string $store,
+        public string $categories,
     )
     {
         //
