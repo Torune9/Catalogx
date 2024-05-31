f1142a82a54de527f10825c35b93de34d348f286
rahman <rahmannurhayadi@gmail.com>
2024-05-31 21:09:11 +0700

    fix : reg auth mid

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\Authenticated;
+use App\Http\Middleware\IsNotAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -12,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->append(Authenticated::class);
+        $middleware->use([
+            Authenticated::class,
+            ]);
+        
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
