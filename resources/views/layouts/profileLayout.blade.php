ccaa52d6b06eeaaae9d75392afc3540cf143392f
rahman <rahmannurhayadi@gmail.com>
2024-05-31 21:10:02 +0700

    fix : fix title

--- resources/views/layouts/profileLayout.blade.php
+++ resources/views/layouts/profileLayout.blade.php
@@ -10,7 +10,7 @@
         href="https://fonts.googleapis.com/css2?family=Barlow:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Cabin:ital,wght@0,400..700;1,400..700&family=Inter:wght@100..900&family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lexend+Deca:wght@100..900&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&family=Rubik+Vinyl&family=Shadows+Into+Light&display=swap"
         rel="stylesheet">
    @vite(['resources/css/app.css','resources/js/app.js'])
-    <title>Document</title>
+    <title>Profile {{ Auth::user()->name}}</title>
     <link rel="stylesheet" href="{{ asset('fontawesome-free-6.5.2-web/css/all.min.css') }}">
 </head>
 <body>
