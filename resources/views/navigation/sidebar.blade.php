1445e1fd8591e9a68c70c6a654b08e6b06cb582a
rahman <rahmannurhayadi@gmail.com>
2024-05-31 21:10:27 +0700

    fix : fix tag lst

--- resources/views/navigation/sidebar.blade.php
+++ resources/views/navigation/sidebar.blade.php
@@ -1,28 +1,28 @@
 <nav class="border-r fixed w-64 h-screen flex flex-col justify-between font-poppins bg-blue-900">
     <ul class="flex flex-col font-semibold h-1/2 justify-center p-8 gap-5">
-        <l1
-            class="hover:bg-black/50 transition-colors duration-300 p-2 rounded text-white cursor-pointer {{ request()->is('users/profile/') ? 'bg-black/30' : 'bg-transparent' }}">
-            <a href="/users/profile/{{ session('auth-user') }}/{{ session('auth-id') }}" class="inline-block w-full">
+        <li
+            class="hover:bg-black/50 transition-colors duration-300 p-2 rounded text-white cursor-pointer {{ request()->routeIs('users.profile') ? 'bg-black/30' : 'bg-transparent' }}">
+            <a href="/users/profile/{{ session('auth-user') }}" class="inline-block w-full">
                 <span>
                     <i class="fa-solid fa-house-user"></i>
                 </span>&nbsp;Profile
             </a>
-        </l1>
-        <l1
-            class="hover:bg-black/50 transition-colors duration-300 p-2 rounded text-white cursor-pointer {{ request()->is('users/profile/catalogs') ? 'bg-black/30' : 'bg-transparent' }}">
+        </li>
+        <li
+            class="hover:bg-black/50 transition-colors duration-300 p-2 rounded text-white cursor-pointer {{ request()->routeIs('users.catalogs') ? 'bg-black/30' : 'bg-transparent' }}">
             <a href="{{ route('users.catalogs') }}" class=" inline-block w-full">
                 <span>
                     <i class="fa-solid fa-square-poll-horizontal"></i>
                 </span>&nbsp;Catalogs
             </a>
-        </l1>
-        <l1 class="hover:bg-black/50 transition-colors duration-300 p-2 rounded text-white cursor-pointer">
+        </li>
+        <li class="hover:bg-black/50 transition-colors duration-300 p-2 rounded text-white cursor-pointer">
             <a href="{{ route('stores.shops') }}" class=" inline-block w-full">
                 <span>
                     <i class="fa-solid fa-store"></i>
                 </span>&nbsp;Stores
             </a>
-        </l1>
+        </li>
     </ul>
     <button class="bg-slate-950 text-sm text-white font-semibold font-poppins p-2 hover:bg-slate-950/50 transition-color duration-300">
         <a href="">
