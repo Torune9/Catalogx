256719daa24a2cdfd45c9016f9d158a0e8c5e49f
rahman <rahmannurhayadi@gmail.com>
2024-05-31 21:11:06 +0700

    fix  : fix param ruote

--- routes/web.php
+++ routes/web.php
@@ -16,8 +16,10 @@ Route::name('users.')->group(function(){
     
     Route::get('/users/profile/catalogs',[ProductController::class,'getProducts'])->name('catalogs');
     
-    Route::get('/users/profile/{username?}/{id?}',[UserController::class,'profile'])->name(('profile'));
-    Route::get('/users/details/{id?}',[UserController::class,'detailUser'])->name('details');
+    Route::get('/users/profile/{username}',[UserController::class,'profile'])->name(('profile'));
+    Route::get('/users/details/{id}',[UserController::class,'detailUser'])->name('details');
+
+    Route::get('/users/logout',[UserController::class,'logout'])->name('logout');
 });
 
 Route::name('auth.')->group(function(){
@@ -26,15 +28,17 @@ Route::name('auth.')->group(function(){
 });
 
 Route::name('product.')->group(function(){
+    Route::get('/product/update/{id}',[ProductController::class,'showUpdateProduct'])->name('showUpdateCatalog');
     Route::post('/product/add',[ProductController::class,'createProduct'])->name('addCatalog');
-    Route::get('/product/update/{id?}',[ProductController::class,'showUpdateProduct'])->name('updateCatalog');
+    Route::put('/product/update/{id}',[ProductController::class,'updateProduct'])->name('updateCatalog');
+    Route::delete('/product/delete/{id}',[ProductController::class,'deleteProduct'])->name('deleteCatalog');
 });
 
 
 Route::name('stores.')->group(function(){
     Route::get('/stores/shops',[StoresController::class,'getShops'])->name('shops');
-    Route::get('/stores/{id?}',[StoresController::class,'getStores'])->name('stores');
-    Route::get('/stores/add',[StoresController::class,'createStore'])->name('stores');
+    Route::get('/stores/{id}',[StoresController::class,'getStores'])->name('stores');
+    Route::post('/stores/add',[StoresController::class,'createStore'])->name('addStore');
 });
 
 
